cd326cd68539290b6b62425fc336d0a05d4e6465
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-05-31 10:28:11 +0700

    initial for weather search with react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      console.log(data);
+      setWeather(data);
+      setQuery("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="app-search">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
+      </div>
+      {weather.main && (
+        <div className="app-widget">
+          <h1 className="weather-city">
+            {weather.name} <sup>{weather.sys.country}</sup>
+          </h1>
+          <div className="weather-temp">
+            {<span>{weather.main.temp}</span>} <sup>°C</sup>
+          </div>
+          <div className="weather-info">
+            <img
+              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description.toUpperCase()}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
